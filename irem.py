6caa2c6e21623d6532e182a4b67f594531638fd0
Zeynep Çevik <95767424+zzeynepcvk@users.noreply.github.com>
2025-07-04 17:40:19 +0300

    Create irem.py

---
+++ irem.py
@@ -0,0 +1,16 @@
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            "overthink" : "aşırı düşünmek",
+            "vibe":"öyle bir hissiyat vermek"
+            }
+
+word = input("Anlamadığınız bir kelime yazın (CRINGE,LOL,overthink,vibe!): ")
+
+
+if word in meme_dict.keys():
+    # Kelime eşleşiyorsa ne yapmalıyız?
+    print(meme_dict[word])
+else:
+    # Kelime eşleşmiyorsa ne yapmalıyız?
+    print("eşleşme yok")
